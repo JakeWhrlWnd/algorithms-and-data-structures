47cb3064a00abf0e52888421661b0dda327164d5
JakeWhrlWnd <justjamescreative@outlook.com>
2024-01-31 22:59:15 -0500

    Added a single-line comment

--- role-playing-game/script.js
+++ role-playing-game/script.js
@@ -17,10 +17,15 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+//initialize buttons
 function goStore() {
     console.log("Go to store.");
 }
 
 function goCave() {
     console.log("Going to cave.");
+}
+
+function fightDragon() {
+    console.log("Fighting dragon.");
 }
