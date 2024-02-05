3e7457977df097907844a3f2b923d72550f28eb2
JakeWhrlWnd <justjamescreative@outlook.com>
2024-02-04 22:52:08 -0500

    Added double quotes around the monster's scream

--- role-playing-game/script.js
+++ role-playing-game/script.js
@@ -35,7 +35,7 @@ const locations = [
     { name: "store", "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"], "button functions": [buyHealth, buyWeapon, goTown], text: "You enter the store." },
     { name: "cave", "button text": ["Fight slime", "Fight fanged beast", "Go to town square"], "button functions": [fightSlime, fightBeast, goTown], text: "You enter the cave. You see some monsters." },
     { name: "fight", "button text": ["Attack", "Dodge", "Run"], "button functions": [attack, dodge, goTown], text: "You are fighting a monster." },
-    { name: "kill monster", "button text": ["Go to town square", "Go to town square", "Go to town square"], "button functions": [goTown, goTown, goTown], text: 'The monster screams \"Arg!\" as it dies. You gain experience points and find gold.' },
+    { name: "kill monster", "button text": ["Go to town square", "Go to town square", "Go to town square"], "button functions": [goTown, goTown, goTown], text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.' },
 ];
 
 //initialize buttons
