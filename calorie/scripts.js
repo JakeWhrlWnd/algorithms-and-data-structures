46181310a099fff1b36bb4f2e7cb9aeb81872aa2
JakeWhrlWnd <justjamescreative@outlook.com>
2024-02-15 17:26:43 -0500

    Added template literals to the addEntry function Added the getCaloriesFromInput function Created an eventlistener for the addEntryButton

--- calorie/scripts.js
+++ calorie/scripts.js
@@ -19,6 +19,24 @@ function isInvalidInput(str) {
 
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll();
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">`;
+    targetInputContainer.insertAdjacentElement('beforeend', HTMLString);
 }
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+
+    for (const item of list) {
+        const currVal = cleanInputString(item.value);
+        const invalidInputMatch = isInvalidInput(currVal);
+
+        if (invalidInputMatch) {}
+    }
+}
+
+addEntryButton.addEventListener('click', addEntry);
 
