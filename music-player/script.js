f0a4c47acdfea47fa1c825f8467259989c132a38
JakeWhrlWnd <justjamescreative@outlook.com>
2024-02-21 07:00:50 -0500

    Updated the functionality of the media player

--- music-player/script.js
+++ music-player/script.js
@@ -117,7 +117,51 @@ const playSong = (id) => {
     userData.currentSong = song;
 
     playButton.classList.add("playing");
+    highlightCurrentSong();
     audio.play();
+};
+
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+
+    playButton.classList.remove("playing");
+    audio.pause();
+};
+
+const playNextSong = () => {
+    if (userData?.currentSong === null) {
+        playSong(userData?.songs[0].id);
+    } else {
+        const currentSongIndex = getCurrentSongIndex();
+        const nextSong = userData?.songs[currentSongIndex + 1];
+        playSong(nextSong.id);
+    }
+};
+
+const playPreviousSong = () => {
+    if (userData?. currentSong === null) {
+        return;
+    } else {
+        const currentSongIndex = getCurrentSongIndex();
+        const previousSong = userData?.songs[currentSongIndex - 1];
+        playSong(previousSong.id);
+    }
+};
+
+const setPlayerDisplay = () => {};
+
+const highlightCurrentSong = () => {
+    const playlistSongElement = document.querySelectorAll(".playlist-song");
+
+    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+
+    playlistSongElements.forEach((songEl) => {
+        songEl.removeAttribute("aria-current");
+    });
+
+    if (songToHighlight) {
+        songToHighlight.setAttribute("aria-current", "true");
+    }
 };
 
 //Function to display the songs in the UI - using an arrow function
@@ -140,6 +184,8 @@ const renderSongs = (arr) => {
     playlistSongs.innerHTML = songsHTML;
 };
 
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
+
 playButton.addEventListener('click', () => {
     if (userData?.currentSong === null) {
         playSong(userData?.songs[0].id);        
@@ -147,6 +193,12 @@ playButton.addEventListener('click', () => {
         playSong(userData?.currentSong.id);
     }
 });
+
+pauseButton.addEventListener('click', pauseSong);
+
+nextButton.addEventListener('click', playNextSong);
+
+previousButton.addEventListener('click', playPreviousSong);
 
 userData?.songs.sort((a, b) => {
     if (a.title < b.title) {
